5fc1b72ad6de6d2ef02f5c65af737f18790cdff2
Nkodem Szusti <szustka94@gmail.com>
2017-04-23 14:19:07 +0200

    Dodane potwierdzenie platnosci i fejkowe okno do placenia

---
+++ ProgramowanieZespolowe/Cinema.View/PotwierdzeniePlatnosci.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Cinema.View
+{
+    /// <summary>
+    /// Interaction logic for PotwierdzeniePlatnosci.xaml
+    /// </summary>
+    public partial class PotwierdzeniePlatnosci : Window
+    {
+        public PotwierdzeniePlatnosci()
+        {
+            InitializeComponent();
+        }
+    }
+}
